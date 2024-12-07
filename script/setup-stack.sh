8c2a4de9bb94a09e12b829d09de2d03f89a34b87
charliecharles03 <cloud9stark@gmail.com>
2024-12-07 23:17:48 +0530

    added in npm setup

--- script/setup-stack.sh
+++ script/setup-stack.sh
@@ -8,7 +8,11 @@ awslocal lambda create-function \
     --function-name localstack-lambda-url-example \
     --runtime nodejs18.x \
     --zip-file fileb://./setup/lambda.zip \
-    --handler index.handler \
+    --handler handler.handler \
     --role arn:aws:iam::000000000000:role/lambda-role
+
+
+awslocal lambda invoke --function-name localstack-lambda-url-example \
+    --payload '{"body": "{\"num1\": \"10\", \"num2\": \"10\"}" }' output.txt
 
 tail -f /dev/null
